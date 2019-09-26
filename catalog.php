9ecfe92be9c03f9278da47a2908593a498661a17
sasikirankakara001 <55825519+sasikirankakara001@users.noreply.github.com>
2019-09-26 11:49:49 +0530

    Add files via upload

---
+++ catalog.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exp1</title>
+	<?php include 'head.php'; ?>
+	<link rel="stylesheet" type="text/css" href="catalog.css">
+	
+</head>
+<body>
+	<div class="card">
+		<img src="jeans3.jpg" alt="Denim Jeans" style="width: 100%">
+		<h1>Tailored Jeans</h1>
+		<p class="Price">$19.99</p>
+		<p>Some text about the jeans......</p>
+		<p><button>Add to cart</button></p>
+
+	</div>
+
+
+</body>
+</html>
