bfe022ca85dbdcb4096057c396a4d9fba13baebe
sasikirankakara001 <55825519+sasikirankakara001@users.noreply.github.com>
2019-09-26 11:26:25 +0530

    Add files via upload

---
+++ head.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Experiment1</title>
+	<link rel="stylesheet" type="text/css" href="exp1.css">
+
+<div class="header">
+	<h1>JNTUK</h1>
+</div>
+<div class="topnav">
+	<a href="head.php">Home</a>
+	<a href="login.php">Login</a>
+	<a href="#">Catalog</a>
+	
+</head>
+<body>
+
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Exp1</title>
+	<?php include 'head.php';?>
+</head>
+<body>
+</body>
+<br><br>
+<body>
+	<form action="login.php">
+		<fieldset>
+			<legend>Personal Information</legend>
+		First Name:<br>
+		<input type="text" name="Firstname"><br>
+		Last Name:<br>
+		<input type="text" name="Lastname"><br>
+		<input type="radio" name="gender" value="male"checked>Male<br>
+		<input type="radio" name="gender" value="female">Female<br>
+		<input type="Submit" name="Submit" value="Submit">
+	</fieldset>
+	</form>
+</body>
+</html>
